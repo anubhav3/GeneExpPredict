f72e5b61c027b9c7723e4d0af5035fac9f4d4084
Anubhav Gupta <anubhav.iiser@gmail.com>
2026-09-18 14:20:15 +0200

    Updated model name and included confusion matrix

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,30 +1,40 @@
-from turtle import mode
 import torch
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, accuracy_score, confusion_matrix
 
 from preprocess import load_data, prepare_data
 from model import create_model
 
-# Load data
+
+# LOAD DATA
 x, y = load_data()
 X_train, X_val, X_test, Y_train, Y_val, Y_test = prepare_data(x, y)
 
-# Load model
-model = create_model()
-model.load_state_dict(torch.load("models/cnn_model.pth"))
+
+# LOAD BEST MODEL
+model = create_model(filters = 16, kernel_size = 3, dense_size = 64)
+model.load_state_dict(torch.load("models/best_model.pth"))
 model.eval()
 
-# Make predictions
-with torch.no_grad():
-    
-    # Raw model output
-    test_output = model(X_test).squeeze()
-    
-    # Convert logits -> Probabilities
-    test_probability = torch.sigmoid(test_output)
-    
-    
-# Calculate AUC
 
-auc = roc_auc_score(Y_test, test_probability.numpy())
-print(f"Test ROC-AUC: {auc:.4f}")
+# MAKE PREDICTIONS
+with torch.no_grad():
+    test_output = model(X_test).squeeze()
+    test_probability = torch.sigmoid(test_output)
+
+
+# TEST ROC-AUC
+auc = roc_auc_score(Y_test.numpy(), test_probability.numpy())
+print(f"Test ROC-AUC = {auc:.4f}")
+
+
+# TEST ACCURACY
+test_prediction = (test_probability >= 0.5).float()
+accuracy = accuracy_score(Y_test.numpy(), test_prediction.numpy())
+print(f"Test Accuracy = {accuracy:.4f}")
+
+
+# CONFUSION MATRIX
+matrix = confusion_matrix(Y_test.numpy(), test_prediction.numpy())
+
+print("\nConfusion Matrix:")
+print(matrix)
